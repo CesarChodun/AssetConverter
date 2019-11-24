9aad8a19c21fa0c509a769a5ac998a519d050371
Cezary Choduń <c.chodun@student.uw.edu.pl>
2019-11-24 12:22:54 +0100

    Update Main.java

--- run/Main.java
+++ run/Main.java
@@ -13,41 +13,29 @@ public class Main {
 	public static final String CONVERTERS_LOCATION_FLAG = "-c";
 	public static final String OUTPUT_FOLDER_LOCATION_FLAG = "-o";
 	
-	
-
 	public static void main(String[] args) {
 		
 		ConverterSet cs = new ConverterSet();
 		
+		// The destination folder
 		String output_folder = "";
 		
 		for(int i = 0; i < args.length; i++) {
 			if(args[i].equals(CONVERTERS_LOCATION_FLAG)) {
-				cs.addConverters(new File(args[i+1]));
+				// Adds converters to the converter set
+				cs.addConverters(new File(args[i + 1]));
 				i++;
 			}
 			else if(args[i].equals(OUTPUT_FOLDER_LOCATION_FLAG)) {
-				output_folder = args[i+1];
+				// Sets the output folder
+				output_folder = args[i + 1];
 				i++;
 			}
 			else {
+				// Converts the folder using loaded converters
 				cs.convert(new File(args[i]), output_folder);
 			}
 		}
-		
-//		PluginLoader loader = new PluginLoader(new File("testPlugin.jar"));
-//		
-//		try {
-//			loader.loadPlugins();
-//			Plugin set = loader.getLoadedPlugin("info.A");
-//			System.out.println(set.getName());
-//		} catch (MalformedURLException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		
-//		System.out.println("Success!");
 	}
 
 }
